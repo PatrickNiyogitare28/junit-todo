4098c743221b37b0e03216908a9c0b3e273757f3
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-11-29 22:51:53 +0200

    chore: added properties

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+insert into task(ID, NAME, START_HOUR, END_HOUR, STATUS) values(102,'Learning Prisma','10:00','11:00',0);
+insert into task(ID, NAME, START_HOUR, END_HOUR, STATUS) values(103,'Learning Blockchain','12:00','13:00',0);
+insert into task(ID, NAME, START_HOUR, END_HOUR, STATUS) values(104,'Reading on Web3','14:00','15:00',0);
+insert into task(ID, NAME, START_HOUR, END_HOUR, STATUS) values(105,'Working on projects','16:00','17:00',0);
